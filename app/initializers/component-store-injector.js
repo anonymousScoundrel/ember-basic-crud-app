2c6765df7d3cbcab0b25c8b57c01452da66cb8c6
Theo Coukoulis <tcoukoulis@gmail.com>
2015-08-06 01:40:32 -0500

    Remove unnecessary file(s)

--- app/initializers/component-store-injector.js
+++
@@ -1,9 +0,0 @@
-// This initializer makes sure that our store is available inside components
-export function initialize(container, application) {
-  application.inject('component', 'store', 'service:store');
-}
-
-export default {
-  name: 'component-store-injector',
-  initialize: initialize
-};
